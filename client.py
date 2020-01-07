cb945bd394e6eda9b5f14ed51c58e9a397c6988f
Thibault Serti <thibault.serti@gmail.com>
2020-01-07 11:45:01 +0100

    Fix BrokenPipeError

--- client.py
+++ client.py
@@ -4,6 +4,7 @@
 import socket
 import sys
 import signal
+import time
 
 def signal_handler(sig, frame):
     connection_with_server.send("END".encode())
@@ -19,13 +20,18 @@ connection_with_server.connect((host, port))
 print(f"Connexion établie avec le serveur sur le {port}")
 
 msg = b""
-while msg != b"END":
-    signal.signal(signal.SIGINT, signal_handler)
-    msg = input("> ")
-    msg = msg.encode()
-    connection_with_server.send(msg)
-    msg_rcv = connection_with_server.recv(1024)
-    print(msg_rcv.decode())
+retry = 0
+while msg != b"END" and retry < 5:
+    retry = 0
+    try:
+        signal.signal(signal.SIGINT, signal_handler)
+        msg = input("> ")
+        msg = msg.encode()
+        connection_with_server.send(msg)
+        msg_rcv = connection_with_server.recv(1024)
+        print(msg_rcv.decode())
+    except BrokenPipeError:
+        continue
 
 print("Connexion fermée")
 connection_with_server.close()
